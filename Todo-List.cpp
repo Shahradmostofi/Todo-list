afa6601631ac20192aea7c0fb84d1949c5aa4fe8
Shahradmostofi <Shahradmostofi57@gmail.com>
2026-01-03 00:16:41 +0330

    declare & define todo class

--- Todo-List.cpp
+++ Todo-List.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include "Date.h"
 using namespace std;
-
 
 enum Status
 {
@@ -20,36 +20,171 @@ class Task
     private:
         string Title;
         string Description;
-        Status status;
-        Olaviat olaviat;
+        Olaviat olaviat;    
         Date createdate;
         Date deadline;
         Date today;
+
+    protected: 
+        Status status;
+        
     public:
-        Task(string = "" , string = "" , Status = NotDone , Olaviat = Medium /* , Date , Date , Date */);
+        Task(string = "" , string = "" , Status = NotDone , int = 1 , Date , Date , Date );
+        void setTitle(string );
+        void setDescription(string );
+        void setolaviat(int );
+        void setstatus(Status );
+        string getTitle();
+        string getDescription();
+        int getolaviat();
+        Status getstatus();
         void ToggleStatus();
         void display();
         bool isbackward();
-};
-Task ::  Task(string T  , string D , Status s , Olaviat o /* , Date c , Date d , Date t */)
+    };
+    Task ::  Task(string T  , string D , Status s , int a  , Date c , Date d , Date t )
+    {
+        setTitle(T) ;
+        setDescription(D) ;
+        setstatus(s);
+        setolaviat(a); 
+        createdate = c ;
+        deadline = d ; 
+        today = t ;
+    };
+void Task :: setTitle(string T)
 {
-    Title = T ;
-    Description = D ;
-    status = s ;
-    olaviat = o ; 
-};
+    cin>>T;
+    this -> Title = T;
+}
+void Task :: setDescription(string D)
+{
+    getline(cin , D);
+    this -> Description = D;
+}
+void Task :: setolaviat(int a)
+{
+    switch (a)
+    {
+    case 1:
+        olaviat = Low;
+        break;
+    case 2:
+        olaviat = Medium;
+        break;
+    case 3:
+        olaviat = High;
+        break;
+    default:
+        olaviat = Low;
+        break;
+    }
+}
+void Task :: setstatus(Status s)
+{
+    this -> status = s;
+}
+string Task :: getTitle()
+{
+    return Title;
+}
+string Task :: getDescription()
+{
+    return Description;
+}
+int Task :: getolaviat()
+{
+    return olaviat;
+}
+Status Task :: getstatus()
+{
+    return status;
+}
 void Task :: ToggleStatus()
 {
     if(status == Done)
-        status =NotDone;
+        status = NotDone;
     else
         status = Done;
 };
 void Task :: display()
 {
-    
+    cout<<"***Title:"<<Title<<"***\n"<<"Description:"<<Description<<"\n"<<"olaviat:"<<olaviat<<"\t"<<"status:"<<status<<"\n";
 };
 bool Task :: isbackward()
 {
-    //return today > deadline;
+    return today > deadline;
 };
+
+
+class TodoList : public Task 
+{
+    private: 
+        vector <Task> tasks;
+    public:
+        void NewTask(Task );
+        void DeleteTask(string ); 
+        void toggletask(string ) ;
+        void ShowAll();
+        void D_NTasks();
+        void Showbackward();
+};
+void TodoList :: NewTask(Task t)
+{
+    tasks.push_back(t);
+}
+void TodoList :: DeleteTask(string s)
+{
+    for(int i=0 ; tasks.size() ; i++)
+    {
+        if(tasks[i].getTitle()== s)
+        {
+            tasks.erase(tasks.begin() + i);
+            break;
+        }
+    }
+}
+void TodoList :: toggletask(string s)
+{
+    for (int i=0 ; i<tasks.size() ; i++) 
+    {
+        if(tasks[i].getTitle() == s)
+        {
+            tasks[i].ToggleStatus();
+            break;
+        }
+    }
+}
+void TodoList :: ShowAll()
+{
+    for(Task t : tasks)
+    {
+        t.display();       
+    }
+}
+void TodoList :: D_NTasks()
+{
+    cout<<"Done tasks is:";
+    for(Task t : tasks)
+    {
+        if(t.getstatus() == Done)
+        {
+            t.display();
+        }
+        else
+        {
+            cout<<"NotDone tasks is:";
+            t.display();
+        }
+    }
+}
+void TodoList :: Showbackward()
+{
+    for(Task t : tasks)
+    {
+        if(t.isbackward())
+        {
+            t.display();
+        }
+    }
+}
